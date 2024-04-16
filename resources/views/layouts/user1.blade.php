18456f66f5fecf950d858f488c7a3bbc82810ffc
ham-123 <hamidtchemoko9@gmail.com>
2024-04-16 16:47:06 +0100

    coorection Commit

---
+++ resources/views/layouts/user1.blade.php
@@ -0,0 +1,121 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from pixydrops.com/linoorhtml/index-6.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 31 Mar 2024 23:11:27 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+	<meta charset="utf-8">
+	<title>Route-SAFE</title>
+	<!-- Stylesheets -->
+	<link rel="preconnect" href="https://fonts.gstatic.com/">
+	<link
+		href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300;400;600;700;800;900&amp;family=Teko:wght@300;400;500;600;700&amp;display=swap"
+		rel="stylesheet">
+	<!-- reey font -->
+	<link rel="stylesheet" href="css/reey-font.css">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/fontawesome-all.css" rel="stylesheet">
+	<link href="css/owl.css" rel="stylesheet">
+	<link href="css/flaticon.css" rel="stylesheet">
+	<link href="css/linoor-icons-2.css" rel="stylesheet">
+	<link href="css/animate.css" rel="stylesheet">
+	<link href="css/jquery-ui.css" rel="stylesheet">
+	<link href="css/jquery.fancybox.min.css" rel="stylesheet">
+	<link href="css/hover.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/jarallax.css">
+	<link rel="stylesheet" href="css/swiper.min.css">
+	<link href="css/custom-animate.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<!-- rtl css -->
+	<link href="css/rtl.css" rel="stylesheet">
+	<!-- Responsive File -->
+	<link href="css/responsive.css" rel="stylesheet">
+
+	<!-- variable update -->
+	<link rel="stylesheet" href="css/variables/index-6.css">
+
+	<!-- Color css -->
+	<link rel="stylesheet" id="jssDefault" href="css/colors/color-10.css">
+
+	<link rel="shortcut icon" href="images/favicon.png" id="fav-shortcut" type="image/x-icon">
+	<link rel="icon" href="images/favicon.png" id="fav-icon" type="image/x-icon">
+
+	<!-- Responsive Settings -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+	<!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+</head>
+
+<body>
+    
+    <nav class="mainmenu-six main-header">
+        <div class="auto-container">
+            <div class="mainmenu-six__inner nav-outer">
+                <nav class="main-menu navbar-expand-md navbar-light">
+                    <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
+                        <ul class="navigation clearfix">
+                            <li class="dropdown megamenu megamenu-clickable megamenu-clickable--toggler"><a
+                                    href="index-main.html">Home</a>
+                               
+                            </li>
+                            <li class="dropdown">
+                                <a href="about.html">About Us</a>
+                             
+                            </li>
+                            <li class="dropdown"><a href="#">Pages</a>
+                               
+                            </li>
+                            <li class="dropdown"><a href="services.html">Services</a>
+                               
+                            </li>
+                            <li class="dropdown"><a href="portfolio.html">Portfolio</a>
+                              
+                            </li>
+                           
+                            <li class="dropdown">
+                                <a href="contact.html">Contact</a>
+                    
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+            </div><!-- /.mainmenu-six__inner -->
+        </div>
+    </nav>
+
+    
+
+
+	@yield('content')
+
+
+
+	<script src="js/jquery.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/TweenMax.js"></script>
+	<script src="js/jquery-ui.js"></script>
+	<script src="js/jquery.fancybox.js"></script>
+	<script src="js/owl.js"></script>
+	<script src="js/mixitup.js"></script>
+	<script src="js/appear.js"></script>
+	<script src="js/wow.js"></script>
+	<script src="js/jQuery.style.switcher.min.js"></script>
+	<script type="text/javascript" src="../../cdnjs.cloudflare.com/ajax/libs/js-cookie/2.1.2/js.cookie.min.js">
+	</script>
+	<script src="js/jquery.easing.min.js"></script>
+	<script src="js/jarallax.min.js"></script>
+	<script src="js/swiper.min.js"></script>
+	<script src="js/custom-script.js"></script>
+
+	<script src="js/lang.js"></script>
+	<script src="../../translate.google.com/translate_a/elementa0d8.js?cb=googleTranslateElementInit"></script>
+	<script src="js/color-switcher.js"></script>
+</body>
+
+
+</html>
