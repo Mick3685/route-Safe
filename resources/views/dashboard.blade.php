6c0d81920e527577f28d1810e159fe0d34812f4d
ham-123 <hamidtchemoko9@gmail.com>
2024-04-18 01:15:45 +0100

    Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,15 +2,142 @@
 
 
 @section('content')
-
-
-<main>
-    <div class="container mt-5">
-        <div class="alert alert-success" role="alert">
-            You're logged in!
+<section class="slider-six">
+    <div class="slider-six__curv">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 122">
+            <path fill="currentColor"
+                d="M0,122.005S80.814,20.129,283.871,2.426C526.255-18.706,550.388,91.2,792.025,102.115c243.565,11.006,299.815-108,520.765-97.662,220.1,10.3,273.86,88.036,415.09,98.26C1878.57,113.62,1920,51.068,1920,51.068V-862.995H0v985Z" />
+        </svg>
+    </div><!-- /.slider-six__curv -->
+    <div class="swiper-container thm-swiper__slider"
+        data-swiper-options='{"slidesPerView": 1, "loop": true, "effect": "fade", "pagination": { "el": "#main-slider-pagination", "type": "bullets", "clickable": true }, "navigation": { "nextEl": "#main-slider__swiper-button-next", "prevEl": "#main-slider__swiper-button-prev" }, "autoplay": { "delay": 5000 }}'>
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="image-layer"
+                    style="background-image: url(images/update-01-10-2021/slider/slider-six-1-1.jpg);">
+                </div>
+                <!-- /.image-layer -->
+                <div class="auto-container">
+                    <div class="slider-six__content">
+                        <p class="slider-six__text"><span>Welcome to best linoor business agency</span></p>
+                        <!-- /.slider-six__text -->
+                        <h2 class="slider-six__title">meet the corporate <br> business agency</h2>
+                        <!-- /.slider-six__title -->
+                        <a href="#" class="slider-six__btn thm-btn__six">Discover More</a>
+                        <!-- /.thm-btn__six -->
+                    </div><!-- /.slider-six__content -->
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="image-layer"
+                    style="background-image: url(images/update-01-10-2021/slider/slider-six-1-2.jpg);">
+                </div>
+                <!-- /.image-layer -->
+                <div class="auto-container">
+                    <div class="slider-six__content">
+                        <p class="slider-six__text"><span>Welcome to best linoor business agency</span></p>
+                        <!-- /.slider-six__text -->
+                        <h2 class="slider-six__title">meet the corporate <br> business agency</h2>
+                        <!-- /.slider-six__title -->
+                        <a href="#" class="slider-six__btn thm-btn__six">Discover More</a>
+                        <!-- /.thm-btn__six -->
+                    </div><!-- /.slider-six__content -->
+                </div>
+            </div>
+        </div>
+        <!-- If we need navigation buttons -->
+        <div class="slider-bottom-box clearfix">
+            <div class="main-slider__nav">
+                <div class="swiper-button-prev" id="main-slider__swiper-button-next">
+                    <span class="fa fa-angle-left"></span>
+                </div>
+                <div class="swiper-button-next" id="main-slider__swiper-button-prev">
+                    <span class="fa fa-angle-right"></span>
+                </div>
+            </div>
         </div>
     </div>
-</main>
+</section>
 
 
-@endsection('content')
+<section class="assurance-automobile">
+    <div class="auto-container">
+        <div class="row">
+            <div class="col-md-12 col-lg-6 wow fadeInLeft" data-wow-duration="1500ms">
+                <div class="assurance-automobile__images">
+                    <img src="images/update-01-10-2021/resources/about-seven-1-1.jpg" alt="">
+                    <img src="images/update-01-10-2021/resources/about-seven-1-2.jpg" alt="">
+                </div><!-- /.assurance-automobile__images -->
+            </div><!-- /.col-md-12 col-lg-6 -->
+            <div class="col-md-12 col-lg-6">
+                <div class="assurance-automobile__content">
+                    <div class="sec-title-six text-start">
+                        <p class="sec-title-six__text"><span>À Propos de Notre Assurance Automobile</span></p>
+                        <!-- /.sec-title-six__text -->
+                        <h2 class="sec-title-six__title">Nous offrons les meilleurs services d'assurance automobile</h2>
+                        <!-- /.sec-title-six__title -->
+                    </div><!-- /.sec-title-six -->
+                    <p class="assurance-automobile__summery">Il existe de nombreuses variations de passages du Lorem Ipsum, mais la majorité ont subi une altération sous une forme ou une autre, simplement du texte sans signification injecté.</p><!-- /.assurance-automobile__summery -->
+                    <h3 class="assurance-automobile__title">Linoor est le leader en assurance automobile numérique.</h3>
+                    <!-- /.assurance-automobile__title -->
+                    <ul class="assurance-automobile__list list-unstyled">
+                        <li>
+                            <i class="flaticon-checked"></i>
+                            Nous sommes des designers professionnels et certifiés
+                        </li>
+                        <li>
+                            <i class="flaticon-checked"></i>
+                            Nous utilisons des matériaux de qualité pour rendre votre assurance fiable
+                        </li>
+                        <li>
+                            <i class="flaticon-checked"></i>
+                            Nous nous soucions de la satisfaction de nos clients
+                        </li>
+                    </ul><!-- /.assurance-automobile__list list-unstyled -->
+                    <a href="about.html" class="assurance-automobile__btn thm-btn__six">En savoir plus</a>
+                </div><!-- /.assurance-automobile__content -->
+            </div><!-- /.col-md-12 col-lg-6 -->
+        </div><!-- /.row -->
+    </div><!-- /.auto-container -->
+</section>
+<!-- /.about-seven -->
+
+<section class="suivi-voiture">
+    <div class="suivi-voiture__curv">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 122">
+            <path fill="currentColor"
+                d="M0,122.005S80.814,20.129,283.871,2.426C526.255-18.706,550.388,91.2,792.025,102.115c243.565,11.006,299.815-108,520.765-97.662,220.1,10.3,273.86,88.036,415.09,98.26C1878.57,113.62,1920,51.068,1920,51.068V-862.995H0v985Z" />
+        </svg>
+    </div><!-- /.suivi-voiture__curv -->
+
+    <div class="auto-container">
+        <div class="sec-title-six text-center">
+            <p class="sec-title-six__text"><span>Suivi de Vos Voitures</span></p>
+            <!-- /.sec-title-six__text -->
+            <h2 class="sec-title-six__title">Informations sur Vos Voitures</h2>
+            <!-- /.sec-title-six__title -->
+        </div><!-- /.sec-title-six -->
+        <div class="row">
+            <!-- Insérez ici vos informations sur vos voitures -->
+            <!-- Exemple -->
+            <div class="col-sm-12 col-md-12 col-lg-4 wow fadeInUp" data-wow-duration="1500ms">
+                <div class="suivi-voiture__item">
+                    <div class="suivi-voiture__image">
+                        <img src="images/update-01-10-2021/cars/car-1.jpg" alt="Voiture 1">
+                    </div><!-- /.suivi-voiture__image -->
+                    <div class="suivi-voiture__content">
+                        <div class="suivi-voiture__icon">
+                            <i class="flaticon-car"></i>
+                        </div><!-- /.suivi-voiture__icon -->
+                        <h3 class="suivi-voiture__title">Voiture 1</h3>
+                        <!-- /.suivi-voiture__title -->
+                        <div class="suivi-voiture__status">À jour</div>
+                        <!-- /.suivi-voiture__status -->
+                    </div><!-- /.suivi-voiture__content -->
+                </div><!-- /.suivi-voiture__item -->
+            </div><!-- /.col-sm-12 col-md-12 col-lg-4 -->
+            <!-- Fin de l'exemple -->
+        </div><!-- /.row -->
+    </div><!-- /.auto-container -->
+</section>
+<!-- /.service-six -->
--- resources/views/layouts/user1.blade.php
+++ resources/views/layouts/user1.blade.php
@@ -7,7 +7,7 @@
 <!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
 <head>
 	<meta charset="utf-8">
-	<title>Route-SAFE</title>
+	<title>Route Safe</title>
 	<!-- Stylesheets -->
 	<link rel="preconnect" href="https://fonts.gstatic.com/">
 	<link
@@ -50,47 +50,187 @@
 </head>
 
 <body>
-    
-    <nav class="mainmenu-six main-header">
-        <div class="auto-container">
-            <div class="mainmenu-six__inner nav-outer">
-                <nav class="main-menu navbar-expand-md navbar-light">
-                    <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
-                        <ul class="navigation clearfix">
-                            <li class="dropdown megamenu megamenu-clickable megamenu-clickable--toggler"><a
-                                    href="index-main.html">Home</a>
-                               
-                            </li>
-                            <li class="dropdown">
-                                <a href="about.html">About Us</a>
-                             
-                            </li>
-                            <li class="dropdown"><a href="#">Pages</a>
-                               
-                            </li>
-                            <li class="dropdown"><a href="services.html">Services</a>
-                               
-                            </li>
-                            <li class="dropdown"><a href="portfolio.html">Portfolio</a>
-                              
-                            </li>
-                           
-                            <li class="dropdown">
-                                <a href="contact.html">Contact</a>
-                    
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
 
-            </div><!-- /.mainmenu-six__inner -->
-        </div>
-    </nav>
+	<div class="page-wrapper">
 
-    
+		<div class="preloader">
+			<div class="icon" style="background-image: url(images/update-01-10-2021/preloader-6.png);"></div>
+		</div>
+
+		<header class="header-six">
+			<div class="auto-container">
+				<div class="header-six__logo">
+					<a href="index-main.html">
+						<img src="images/update-01-10-2021/logo-6-dark.png" width="134"
+							alt="Linoor - DIgital Agency HTML Template | Corporate Agency | Home 06 ">
+					</a>
+
+					<div class="mobile-nav-toggler"><span class="icon flaticon-menu-2"></span><span
+							class="txt">Menu</span></div>
+				</div>
+
+			</div>
+		</header>
+
+		<nav class="mainmenu-six main-header">
+			<div class="auto-container">
+				<div class="mainmenu-six__inner nav-outer">
+					<nav class="main-menu navbar-expand-md navbar-light">
+						<div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
+							<ul class="navigation clearfix">
+								<li class="dropdown megamenu megamenu-clickable megamenu-clickable--toggler"><a
+										href="index-main.html">Accueil</a>
+									
+								</li>
+								<li class="dropdown">
+									<a href="about.html">Status</a>
+								
+								</li>
+								<li class="dropdown"><a href="#">Historique</a>
+									
+								</li>
+								<li class="dropdown"><a href="services.html">Paiement</a>
+									
+								</li>
+								<li class="dropdown"><a href="portfolio.html">Facture</a>
+									
+								</li>
+								<li class="dropdown"><a href="shop.html">Notification</a>
+									
+								</li>
+								<li class="dropdown"><a href="blog-grid.html">A Propos</a>
+									
+								</li>
+						
+							</ul>
+						</div>
+					</nav>
+
+					<div class="mainmenu-six__right">
+						
+						<a href="{{ route('profile.edit') }}" class="text-truncate" style="font-size: 20px;">
+							{{ Auth::user()->name }}</a>
+					</div>
+				</div>
+			</div>
+		</nav>
+
+		<!--Mobile Menu-->
+		<div class="side-menu__block">
+
+
+			<div class="side-menu__block-overlay custom-cursor__overlay">
+				<div class="cursor"></div>
+				<div class="cursor-follower"></div>
+			</div><!-- /.side-menu__block-overlay -->
+			<div class="side-menu__block-inner ">
+				<div class="side-menu__top justify-content-end">
+
+					<a href="#" class="side-menu__toggler side-menu__close-btn"><img src="images/icons/close-1-1.png"
+							alt=""></a>
+				</div><!-- /.side-menu__top -->
+
+
+				<nav class="mobile-nav__container">
+					<!-- content is loading via js -->
+				</nav>
+				<div class="side-menu__sep"></div><!-- /.side-menu__sep -->
+				<div class="side-menu__content">
+					<p>Linoor is a premium Template for Digital Agencies, Start Ups, Small Business and a wide range of
+						other agencies.</p>
+					<p><a href="mailto:needhelp@linoor.com">needhelp@linoor.com</a> <br> <a href="tel:888-999-0000">888
+							999 0000</a></p>
+					<div class="side-menu__social">
+						<a href="#"><i class="fab fa-facebook-square"></i></a>
+						<a href="#"><i class="fab fa-twitter"></i></a>
+						<a href="#"><i class="fab fa-instagram"></i></a>
+						<a href="#"><i class="fab fa-pinterest-p"></i></a>
+					</div>
+				</div><!-- /.side-menu__content -->
+			</div><!-- /.side-menu__block-inner -->
+		</div><!-- /.side-menu__block -->
+
+		<!--Search Popup-->
+		<div class="search-popup">
+			<div class="search-popup__overlay custom-cursor__overlay">
+				<div class="cursor"></div>
+				<div class="cursor-follower"></div>
+			</div><!-- /.search-popup__overlay -->
+			<div class="search-popup__inner">
+				<form action="#" class="search-popup__form">
+					<input type="text" name="search" placeholder="Type here to Search....">
+					<button type="submit"><i class="fa fa-search"></i></button>
+				</form>
+			</div><!-- /.search-popup__inner -->
+		</div><!-- /.search-popup -->
+
+
 
 
 	@yield('content')
+
+
+
+
+
+	<section class="footer-six">
+		<div class="auto-container">
+			<div class="row">
+				<div class="col-sm-12 col-md-12 col-lg-3">
+					<div class="footer-six__widget footer-six__about">
+						<a href="index.html">
+							<img src="images/update-01-10-2021/logo-6-light.png" width="135" alt="">
+						</a>
+						<p class="footer-six__about__text">
+							The new corporate agency theme
+						</p><!-- /.footer-six__about__text -->
+						<div class="footer-six__social">
+							<a href="#"><i class="fab fa-twitter"></i></a>
+							<a href="#"><i class="fab fa-facebook"></i></a>
+							<a href="#"><i class="fab fa-pinterest-p"></i></a>
+							<a href="#"><i class="fab fa-instagram"></i></a>
+						</div><!-- /.footer-six__social -->
+					</div><!-- /.footer-six__widget -->
+				</div><!-- /.col-sm-12 col-md-12 col-lg-3 -->
+				<div class="col-sm-12 col-md-12 col-lg-4">
+					<div class="footer-six__widget footer-six__links">
+						<h3 class="footer-six__title">Explore</h3><!-- /.footer-six__title -->
+						<div class="footer-six__links__list">
+							<ul class="footer-six__links__list__item">
+								<li><a href="#">About</a></li>
+								<li><a href="#">Meet Our Team</a></li>
+								<li><a href="#">Our Projects</a></li>
+								<li><a href="#">Latest News</a></li>
+								<li><a href="#">Contact</a></li>
+							</ul><!-- /.footer-six__links__list__item -->
+							<ul class="footer-six__links__list__item">
+								<li><a href="#">Support</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+								<li><a href="#">Terms & Condition</a></li>
+								<li><a href="#">Help</a></li>
+							</ul><!-- /.footer-six__links__list__item -->
+						</div><!-- /.footer-six__links__list -->
+					</div><!-- /.footer-six__widget footer-six__links -->
+				</div><!-- /.col-sm-12 col-md-12 col-lg-4 -->
+				<div class="col-sm-12 col-md-12 col-lg-5">
+					<div class="footer-six__widget footer-six__newsletter">
+						<h3 class="footer-six__title">Newsletter</h3><!-- /.footer-six__title -->
+						<form action="#" class="footer-six__newsletter__form">
+							<input type="text" placeholder="Email address">
+							<button type="submit"><i class="fa fa-paper-plane"></i></button>
+							<p>I agree to all <a href="#">terms and policies</a> </p>
+						</form><!-- /.footer-six__newsletter__form -->
+					</div><!-- /.footer-six__widget footer-six__newsletter -->
+				</div><!-- /.col-sm-12 col-md-12 col-lg-5 -->
+			</div><!-- /.row -->
+		</div><!-- /.auto-container -->
+	</section><!-- /.footer-six -->
+	<section class="footer-six__copyright">
+		<div class="auto-container">
+			<p>© Copyright 2022 by Layerdrops.com</p>
+		</div><!-- /.auto-container -->
+	</section><!-- /.footer-six__copyright -->
+
 
 
 
@@ -105,13 +245,11 @@
 	<script src="js/appear.js"></script>
 	<script src="js/wow.js"></script>
 	<script src="js/jQuery.style.switcher.min.js"></script>
-	<script type="text/javascript" src="../../cdnjs.cloudflare.com/ajax/libs/js-cookie/2.1.2/js.cookie.min.js">
-	</script>
+	<script type="text/javascript" src="../../cdnjs.cloudflare.com/ajax/libs/js-cookie/2.1.2/js.cookie.min.js"></script>
 	<script src="js/jquery.easing.min.js"></script>
 	<script src="js/jarallax.min.js"></script>
 	<script src="js/swiper.min.js"></script>
 	<script src="js/custom-script.js"></script>
-
 	<script src="js/lang.js"></script>
 	<script src="../../translate.google.com/translate_a/elementa0d8.js?cb=googleTranslateElementInit"></script>
 	<script src="js/color-switcher.js"></script>
